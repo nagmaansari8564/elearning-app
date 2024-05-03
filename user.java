f870bb586c7ba471b8da75b0aa963d754dc09a8f
Nagma <nagmaansari8564@gmail.com>
2024-05-03 09:32:58 +0530

    its done before 101

--- user.java
+++ user.java
@@ -6,5 +6,7 @@ public class user {
     public static void main(String[] args) {
         
         System.out.println("NAgma");
+
+        System.out.println("i have done this worke before 101");
     }
 }
